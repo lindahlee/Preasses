e08a6f2956d71b10cd137eb52c42c1fcce2a3798
lindahlee <154841588+lindahlee@users.noreply.github.com>
2025-01-23 15:32:59 -0500

    Initial Commit

---
+++ PreassesTests/PreassesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreassesTests.swift
+//  PreassesTests
+//
+//  Created by Linda Lee on 1/23/25.
+//
+
+import Testing
+@testable import Preasses
+
+struct PreassesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
